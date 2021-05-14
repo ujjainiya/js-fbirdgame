c386770cd00a1fc41a660a524634817395da30a1
ujjainiya <80682425+ujjainiya@users.noreply.github.com>
2021-05-14 14:19:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+
+hole.addEventListener('animationiteration', () => {
+var random=-((Math.random()+300)+150);
+hole.style.top= random + "px";
+});
